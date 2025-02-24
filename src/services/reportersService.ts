d447a2b6747ae457eda851b678407f59c8f1031f
WesleyMaciel2510 <wesley.2510@hotmail.com>
2025-02-24 19:20:51 -0300

    fix:ReportersService working

--- src/services/reportersService.ts
+++ src/services/reportersService.ts
@@ -3,14 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-//const ELASTIC_URL = process.env.ELASTIC_URL as string;
-//const ELASTIC_TOKEN = process.env.ELASTIC_TOKEN as string;
-//const ELASTIC_INDEX = process.env.ELASTIC_INDEX as string;
-
-// Configurações do Elasticsearch
-const ELASTIC_URL = "https://elastic.nelk.easyjur.com";
-const ELASTIC_TOKEN = "ZktTWDlwUUJXZXBpZ1lyOHMtSWs6VGpXX3Bsby1RTUd1MXVRbjJVRVRvUQ==";
-const ELASTIC_INDEX = "search-juris-tst";
+const ELASTIC_URL = process.env.ELASTIC_URL as string;
+const ELASTIC_TOKEN = process.env.ELASTIC_TOKEN as string;
+const ELASTIC_INDEX = process.env.ELASTIC_INDEX as string;
 
 export class ReportersService {
   public async getReporters(): Promise<string[]> {
@@ -19,8 +14,8 @@ export class ReportersService {
       const response = await axios.post(
         `${ELASTIC_URL}/${ELASTIC_INDEX}/_search`, // URL completa para a busca no índice
         {
-          size: 10000, // Aumentar o tamanho para recuperar todos os relatores (ajuste conforme necessário)
-          _source: ["reporter"], // Especificar o campo que queremos recuperar
+          size: 10000, // Aumentar o tamanho para recuperar todos os relatores
+          _source: ["relator"], // Especificar o campo que queremos recuperar
           query: {
             match_all: {}, // Buscar todos os documentos no índice
           },
@@ -37,19 +32,17 @@ export class ReportersService {
       const hits = response.data.hits.hits;
 
       if (!hits || hits.length === 0) {
-        console.log("Nenhum relator encontrado no índice.");
         return []; // Retorna um array vazio se não houver resultados
       }
 
       // Mapeando os resultados para extrair os nomes dos relatores
-      const reporters: string[] = hits.map(
-        (hit: any) => hit._source.reporter as string // Extrai o campo 'reporter' de cada documento
-      );
+      const reporters: string[] = hits
+        .map((hit: any) => hit._source.relator) // Extrai o campo 'relator' de cada documento
+        .filter((relator: string | undefined) => relator !== undefined && relator !== null); // Filtra valores undefined ou null
 
       // Remover duplicatas e ordenar os relatores alfabeticamente
       const uniqueReporters = [...new Set(reporters)].sort();
 
-      console.log("Relatores encontrados:", uniqueReporters); // Log para depuração
       return uniqueReporters; // Retorna a lista de relatores únicos e ordenados
     } catch (error: any) {
       console.error("Erro ao buscar relatores:", error.response?.data || error.message);
@@ -57,4 +50,3 @@ export class ReportersService {
     }
   }
 }
-
